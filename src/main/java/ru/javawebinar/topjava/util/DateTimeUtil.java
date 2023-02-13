85ee16c8e7f6726a17a2b8ca1f5172294424b1f6
Gromyko Alexander <gromyko.alexander@gmail.com>
2023-02-13 20:55:02 +0400

    HW2 Added generic to TimeUtil.isBetweenHalfOpen

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,17 +1,12 @@
 package ru.javawebinar.topjava.util;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
-    }
-
-    public static boolean isBetweenHalfOpen(LocalDateTime ldt, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+    public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T ldt, T startDateTime, T endDateTime) {
         return ldt.compareTo(startDateTime) >= 0 && ldt.compareTo(endDateTime) <= 0;
     }
 
